f32d831d1bf54613c078690848c1308a97742d7a
xCyal <manuvilla4@correo.ugr.es>
2022-01-01 21:41:27 +0100

    Corregir errores python en tablero

--- tracker/tablero.py
+++ tracker/tablero.py
@@ -1,7 +1,7 @@
 class Tablero:
 
     def __init__(self, posicion_tablero):
-        self._posicion = piezas_tablero
+        self._posicion = posicion_tablero
 
     def get_posicion(self):
         return self._posicion
