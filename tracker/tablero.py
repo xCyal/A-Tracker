b3909c8c67009d429a6d8e8c1de5643964809c98
xCyal <manuvilla4@correo.ugr.es>
2022-01-15 14:55:01 +0100

    Crear clase tablero y avanzar #16
    Crea la clase tablero necesaria para #16, el cual a su vez es requerido para #3

---
+++ tracker/tablero.py
@@ -0,0 +1,40 @@
+class Tablero:
+    '''
+    Clase tablero, encargada de almacenar una posicion unica de las piezas en un tablero de ajedrez
+    
+    Atributos
+    ---------
+    _posicion : list[pieza]
+        Variable que almacena hasta MAX_PIEZAS piezas que representan el tablero en una situación concreta
+    MAX_PIEZAS : int
+    	Variable de clase que indica el maximo de piezas que puede almacenar un tablero.
+    '''
+    MAX_PIEZAS = 64;
+    
+    def __init__(self, posicion_tablero):
+        
+        '''
+        Constructor de la clase tablero, inicializa el tablero solo si hay 64 casillas, en caso contrario lo 
+        deja vacío.
+        
+        Atributos
+        ---------
+        posicion_tablero : list[pieza]
+             Argumento que proporciona una lista de 64 casillas para el tablero con la pieza que contiene.
+        '''
+        if len(posicion_tablero) == Tablero.MAX_PIEZAS:
+            self._posicion = posicion_tablero
+        else:
+            self._posicion = []
+            
+    @property
+    def posicion(self):
+        """
+        Property de posicion (Getter)
+        """
+        print("Get de _posicion")
+        return self._posicion
+
+
+        
+        
