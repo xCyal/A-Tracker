3012679f2a2edb2ab796f67ee71cb7e70873975d
xCyal <manuvilla444@gmail.com>
2022-02-01 14:03:09 +0100

    Añadir task para lanzar tests en docker close #32

--- tasks.py
+++ tasks.py
@@ -38,5 +38,16 @@ def test(c):
 def dbuild(c):
 	"""
 	Task encargada de construir el contenedor a partir de el repositorio del proyecto
+	
+	Lanza Docker para crear el contenedor "xcyal/tracker"
 	"""
 	run("docker build --tag xcyal/tracker .")
+	
+@task
+def drun(c):
+	"""
+	Task encargada de lanzar los tests unitarios dentro del contenedor de pruebas
+	
+	Lanza Docker con el contenedor previamente creado para lanzar los tests unitarios
+	"""
+	run("docker run -t -v `pwd`:/app/test xcyal/tracker")
